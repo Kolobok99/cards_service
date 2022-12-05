a6114b5012267e1eb5e535d059f9b0ef68d86ff3
john <izolotavin99@gmail.com>
2022-12-05 12:07:21 +0300

    1) apps.models Card added 2) first migration

--- backend/apps/cards/models.py
+++ backend/apps/cards/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Card(models.Model):
+    """Модель Бонусных карт (карт лояльности, кредитный карт и т.д.)"""
+
+    STATUSES = (
+        ('N', 'NOT_ACTIVATED'),
+        ('A', 'ACTIVATED'),
+        ('O', 'OVERDUE'),
+    )
+
+    series = models.CharField("Cерия", max_length=16)
+    number = models.CharField("Номер", max_length=16)
+
+    issue_date = models.DateTimeField("Дата выпуска")
+    expiration_date = models.DateTimeField("Дата окончания действия")
+    use_date = models.DateTimeField("Дата использования")
+
+    balance = models.FloatField("Баланс")
+
+    status = models.CharField("Статус", choices=STATUSES, max_length=1)
+
+    def __str__(self):
+        return self.number
+
+    class Meta:
+        verbose_name = 'Бонусная карта'
+        verbose_name_plural = 'Бонусные карты'
